a568c8b415605e2bdf56f31a799c78127b582f2d
jonathanbataire <jonathanbataire@gmail.com>
2020-03-18 08:28:40 +0300

    toast button

--- app/src/main/java/com/example/medcom/MainActivity.java
+++ app/src/main/java/com/example/medcom/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.medcom;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mTopToolbar;
+    private Button hospital,fire,police;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +22,34 @@ public class MainActivity extends AppCompatActivity {
         mTopToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mTopToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+        hospital = findViewById(R.id.hospital);
+        police = findViewById(R.id.police);
+        fire = findViewById(R.id.fire);
+        hospital.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, MainActivity2.class);
+                myIntent.putExtra("message", "hospital");
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
+        fire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, MainActivity2.class);
+                myIntent.putExtra("message", "fire");
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+        police.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, MainActivity2.class);
+                myIntent.putExtra("message", "police");
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/medcom/MainActivity2.java
@@ -0,0 +1,18 @@
+package com.example.medcom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        Toast.makeText(this,intent.getStringExtra("message"),Toast.LENGTH_LONG).show();
+    }
+}
